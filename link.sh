c54fc8ea1e32f10048bf61e602ddbb48905beb9f
alextilot <alextilot@gmail.com>
2024-11-18 08:11:31 -0600

    feat: remove styling config before link

--- link.sh
+++ link.sh
@@ -3,9 +3,13 @@
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # zsh
+rm -f ~/.zshrc
 ln -s ${BASEDIR}/.zshrc ~/.zshrc
+rm -f ~/.zsh_plugins.txt
 ln -s ${BASEDIR}/.zsh_plugins.txt ~/.zsh_plugins.txt
+rm -f ~/.p10k.zsh
 ln -s ${BASEDIR}/.p10k.zsh ~/.p10k.zsh
+rm -f ~/.aliases.zsh
 ln -s ${BASEDIR}/.aliases.zsh ~/.aliases.zsh
 
 # git
