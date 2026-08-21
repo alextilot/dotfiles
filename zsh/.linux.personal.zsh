fe2ed02c3dcc9a577c4603bc7b4589f65d6bc189
alextilot <alextilot@gmail.com>
2026-08-20 19:50:34 -0500

    feat: add Go binary path to personal zsh configuration

--- zsh/.linux.personal.zsh
+++ zsh/.linux.personal.zsh
@@ -1,2 +1,3 @@
 # Linux + DOTFILES_PROFILE=personal only. Sourced by .zshrc after linux.zsh.
 # Put non-secret, personal-only config here (aliases, env vars, tool defaults).
+[[ -d /usr/local/go/bin ]] && export PATH="/usr/local/go/bin:$PATH"
